914ed5d7d942d7a3d6d7a3220bbe78908e0a457c
rashid-090 <rashidpk090@gmail.com>
2026-01-02 15:27:35 +0530

    fix code

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
